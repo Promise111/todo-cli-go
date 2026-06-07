9cd276caa0be44643dcd3fc58959af67b24b5402
Promise <promiseihunna@gmail.com>
2026-06-07 21:35:38 +0100

    Standardize command flag names in cmd.go to lowercase and update CompletedAt timestamp in todos.json

--- cmd.go
+++ cmd.go
@@ -19,11 +19,11 @@ type CmdFlags struct {
 func NewCmdFlags() *CmdFlags {
 	cf := CmdFlags{}
 
-	flag.StringVar(&cf.Add, "Add", "", "Add a new todo, specify title")
-	flag.StringVar(&cf.Edit, "Edit", "", "Edit a todo, specify index and title. id:new_title")
-	flag.IntVar(&cf.Del, "Del", -1, "Specify a todo by index to delete")
-	flag.IntVar(&cf.Toggle, "Toggle", -1, "Specify a todo by index to toggle")
-	flag.BoolVar(&cf.List, "List", false, "List all todos")
+	flag.StringVar(&cf.Add, "add", "", "Add a new todo, specify title")
+	flag.StringVar(&cf.Edit, "edit", "", "Edit a todo, specify index and title. id:new_title")
+	flag.IntVar(&cf.Del, "del", -1, "Specify a todo by index to delete")
+	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify a todo by index to toggle")
+	flag.BoolVar(&cf.List, "list", false, "List all todos")
 
 	flag.Parse()
 
